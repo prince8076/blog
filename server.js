21b593bffe154b84d1fe2422c25549a8275507b7
prince8076 <rahulsingh00c@gmail.com>
2024-07-04 12:36:36 +0530

    done

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const colors = require('colors');
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
