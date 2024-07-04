36518bfa018a2162464b4d2518e93a96ea3d1d0c
prince8076 <rahulsingh00c@gmail.com>
2024-07-04 12:38:43 +0530

    done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const colors = require('colors');
+const PORT = process.env.PORT || 5000;
 
 
 // Middleware
@@ -11,3 +12,14 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
+app.get("/", (req, res) => {
+    res.status(200).send({
+        "message": "Welcome to the API"
+    });
+})
+
+//Listen
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`.yellow.bold);
+    console.log(`Press Ctrl+C to stop server`.yellow.bold);
+})
